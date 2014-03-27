d993200bfcbf8b2b7884b8779e0f95a966b9e229
nwagensonner <n.wagensonner@voycer.com>
2014-03-27 16:54:46 +0100

    removes unused deps

--- deps.sh
+++ deps.sh
@@ -2,8 +2,5 @@
 
 go get code.google.com/p/graphics-go/graphics
 go get launchpad.net/gocheck
-go get github.com/kr/godep 
 go get labix.org/v2/mgo 
 go get github.com/gorilla/mux
-go get github.com/gorilla/context
-go get github.com/gorilla/sessions
