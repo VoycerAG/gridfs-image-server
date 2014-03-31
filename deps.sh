a73ea57352c3987224d18b4729f8cff8867a513d
nwagensonner <n.wagensonner@voycer.com>
2014-03-31 16:42:58 +0200

    Fixes deps.sh

--- deps.sh
+++ deps.sh
@@ -5,4 +5,5 @@ go get code.google.com/p/graphics-go/graphics
 go get github.com/nfnt/resize
 go get launchpad.net/gocheck
 go get labix.org/v2/mgo 
-go get github.com/gorilla/muxgo get code.google.com/p/go.tools/cmd/cover
+go get github.com/gorilla/mux
+go get code.google.com/p/go.tools/cmd/cover
