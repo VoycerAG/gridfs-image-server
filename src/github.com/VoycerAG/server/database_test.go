04f2a04566af890280bec79bf2120256e7b82b61
Stefan Verhey <s.verhey@voycer.com>
2014-04-01 14:15:02 +0200

    Add a test for the FindImageByParentFilename method.

---
+++ src/github.com/VoycerAG/server/database_test.go
@@ -0,0 +1,50 @@
+package server
+
+
+import (
+	. "launchpad.net/gocheck"
+	"labix.org/v2/mgo"
+//	"labix.org/v2/mgo/bson"
+//	"image"
+	"os"
+	"io"
+)
+
+// Checker: IsNil, ErrorMatches, Equals, HasLen, FitsTypeof, DeepEquals, NotNil, Not(Checker)
+// Bootstrap unit test suite.
+type DatabaseTestSuite struct{}
+
+var _ = Suite(&DatabaseTestSuite{})
+
+// SetUpTest creates test datasets to the local mongodb server.
+func (s *DatabaseTestSuite) SetUpTest(c *C) {
+	Connection, _ = mgo.Dial("localhost")
+	gridfs := Connection.DB("unittest").GridFS("fs")
+
+	// create a new file
+	gridfsfile, _ := gridfs.Create("image.jpg")
+
+	// set a jpeg image to gridfs
+	testJpeg, _ = os.Open("/../testdata/image.jpg")
+	defer testJpeg.Close()
+	io.Copy(gridfsfile, testJpeg)
+
+	gridfsfile.Close()
+}
+
+// TearDownTest removes the created test file.
+func (s *DatabaseTestSuite) TearDownTest(c *C) {
+	Connection, _ = mgo.Dial("localhost")
+	Connection.DB("unittest").DropDatabase()
+}
+
+// TestFindImageByParentFilename tests the FindImageByParentFile func
+func (s *DatabaseTestSuite) TestFindImageByParentFilename(c *C) {
+	Connection, _ = mgo.Dial("localhost")
+	gridfs := Connection.DB("unittest").GridFS("fs")
+
+	file, err := FindImageByParentFilename("image.jpg", nil, gridfs)
+
+	c.Assert(file, NotNil)
+	c.Assert(err, IsNil)
+}
