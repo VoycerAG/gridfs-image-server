0f3c88686e1c1162e87c3c9a304da803c69b37be
n.wagensonner <n.wagensonner@manyminds.de>
2015-10-23 07:23:48 +0200

    Remove historical build script

--- build-linux.sh
+++
@@ -1,4 +0,0 @@
-CROSS_COMPILE=~/golang-crosscompile/crosscompile.bash
-TARGET_FILENAME=image-server.linux.x64
-
-source $CROSS_COMPILE && go-linux-amd64 build && mv VoycerGo $TARGET_FILENAME && echo $TARGET_FILENAME " Successful created" 
