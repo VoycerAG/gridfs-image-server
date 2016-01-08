4b9cd993c6d534b906904b4a675e63931db2cfac
n.wagensonner <n.wagensonner@manyminds.de>
2016-01-08 21:59:37 +0100

    Remove useless output

--- application.go
+++ application.go
@@ -4,15 +4,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 )
 
 // main starts the server and returns an invalid result as exit code
 func main() {
 	flag.Parse()
-
-	fmt.Printf("%v", configurationFilepath)
 
 	if *configurationFilepath == "" {
 		log.Fatal("configuration must be given")
