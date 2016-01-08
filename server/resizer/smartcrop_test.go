edabed9c13f8b4fe60d16c26b9516d66096e0a64
n.wagensonner <n.wagensonner@manyminds.de>
2016-01-08 14:33:21 +0100

    Improved testcase

--- server/resizer/smartcrop_test.go
+++ server/resizer/smartcrop_test.go
@@ -27,6 +27,15 @@ const (
 	haarCascade  = "../../scripts/haarcascade_frontalface_alt.xml"
 )
 
+type format struct {
+	Height int
+	Width  int
+}
+
+func (f format) String() string {
+	return fmt.Sprintf("%dx%d", f.Height, f.Width)
+}
+
 var _ = Describe("Smartcrop testsuite", func() {
 	loadImage := func(path string) (image.Image, string, error) {
 		fp, err := os.Open(path)
@@ -59,38 +68,47 @@ var _ = Describe("Smartcrop testsuite", func() {
 		return nil
 	}
 
+	formats := []format{
+		format{Width: 220, Height: 480},
+		format{Width: 500, Height: 500},
+		format{Width: 400, Height: 200},
+		format{Width: 50, Height: 50},
+	}
+
 	Measure("it will generate multiple face detected images", func(b Benchmarker) {
 		b.Time("runtime", func() {
 			resizer := NewSmartcrop(haarCascade, nil)
 			facesFound := 0
 			noFacesFound := 0
 			errors := 0
-			dstWidth := 300
-			dstHeight := 294
+			var f format
+			f, formats = formats[len(formats)-1], formats[:len(formats)-1]
 
-			err := filepath.Walk(inputFolder, func(path string, f os.FileInfo, err error) error {
-				if f.IsDir() || filepath.Base(path) == ".gitkeep" {
+			err := filepath.Walk(inputFolder, func(path string, fi os.FileInfo, err error) error {
+				if fi.IsDir() || filepath.Base(path) == ".gitkeep" {
 					return nil
 				}
 
 				image, format, err := loadImage(path)
 				if err != nil {
+					log.Printf("Error at %s\n", path)
 					errors++
 					return nil
 				}
 
-				output, err := resizer.Resize(image, dstWidth, dstHeight)
+				output, err := resizer.Resize(image, f.Width, f.Height)
 				if err != nil {
 					if err == ErrNoFacesFound {
 						noFacesFound++
 						return nil
 					}
 
+					log.Printf("Error at %s\n", path)
 					errors++
 					return nil
 				}
 
-				outputPath := strings.Replace(path, "testimages", "testimages_output", 1)
+				outputPath := strings.Replace(path, "testimages/", "testimages_output/"+f.String()+"_", 1)
 				log.Printf("Writing %s\n", outputPath)
 				saveImage(outputPath, format, output)
 				facesFound++
@@ -101,5 +119,5 @@ var _ = Describe("Smartcrop testsuite", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(errors).To(Equal(0))
 		})
-	}, 1)
+	}, len(formats))
 })
